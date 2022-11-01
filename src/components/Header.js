cc018333e00b9bfbde8fa9344cdc03130cfee4d2
Joshua Diaz <jad24096@gmail.com>
2022-10-31 22:18:28 -0400

    minor css changes to contact page adding mailto for email link.

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,9 +5,9 @@ export default function Header() {
         <header id='header'>
                 <a href='/'><img id='plate'src={Icon} alt='Joshua Diaz' ></img></a>
             <nav>
-                <a href='/about'><button className='navButton'>About me</button></a>
-                <a href='/projects'><button className='navButton'>Projects</button></a>
-                <a href='/contact'><button className='navButton'>Contact</button></a>
+                <a href='/about'><button className='navButton'>ABOUT ME</button></a>
+                <a href='/projects'><button className='navButton'>PROJECTS</button></a>
+                <a href='/contact'><button className='navButton'>CONTACT</button></a>
             </nav>
         </header>
     )
--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -7,7 +7,7 @@ export default function Contact() {
     <>
     <Header/>
     <p id='contact'>
-      Email: jad24096@gmail.com<br/>
+    <a href="mailto:jad24096@gmail.com"><button className='navButton'>SEND EMAIL</button></a><br/>
       Github: https://github.com/Jdiaz240<br/>
     </p>
     </>
