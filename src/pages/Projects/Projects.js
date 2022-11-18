2f1f12920232dce1f7383d148d4ff1cf413bcbc0
Joshua Diaz <jad24096@gmail.com>
2022-11-18 15:46:27 -0500

    added another application to the projects tab.

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -5,6 +5,7 @@ import Applications from '../../components/Applications'
 import Quiz from '../../assets/quizImg.png'
 import Palindrome from '../../assets/palindrome.png'
 import WordGuess from '../../assets/wordGuess.png'
+import WeatherApp from '../../assets/weather.png'
 
 export default function Projects() {
   const [Frame, setFrame] = useState('')
@@ -21,6 +22,7 @@ export default function Projects() {
           <Applications src={WordGuess} alt={'Word Guess Game'} setFrame={setFrame} projLink={'https://jdiaz240.github.io/wordGuess/'} />
           <Applications src={Quiz} alt={'MK Quiz'} setFrame={setFrame} projLink={'https://jdiaz240.github.io/quiz/'} />
           <Applications src={Palindrome} alt={'Palindrome Checker'} setFrame={setFrame} projLink={'https://jdiaz240.github.io/palindrome/'} />
+          <Applications src={WeatherApp} alt={'weather App'} setFrame={setFrame} projLink={'https://jdiaz240.github.io/weatherapp/'} />
         </div> 
       </div>  
     </div>
