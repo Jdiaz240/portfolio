22c7fa929c7713b90193153eac9c4c4e1926dfbc
Joshua Diaz <jad24096@gmail.com>
2022-12-09 20:28:13 -0500

    changed about me paragraph.

--- src/pages/AboutMe/AboutMe.js
+++ src/pages/AboutMe/AboutMe.js
@@ -9,15 +9,17 @@ export default function AboutMe() {
       <Header />
       <div className='bioContainer'>
         <aside>
-          <img id='test' alt='profile' src={Profile}></img>
+          <img id='profilePic' alt='profile' src={Profile}></img>
           </aside>
-          <p id='biography'>Hi! If it wasn't obivous from the home page my name is Joshua Diaz, but <em>Josh</em> is fine.
-            I was born in New York, but raised in a small town in Central Flordia. I have an A.A. degree from Valencia college
-            here in Central Florida and Certificate of completion from the UCF coding bootcamp also from here in Central Florida.
-            It was in this bootcamp where I learned all of the fundamentals of javascript and how to create Full Stack Projects using the MERN Stack.
-            I am currently open for work. If you click on the 'Projects' button above you'll be able to see the projects I've completed that
-            I'm most proud of. Feel Free to look around the page, and don't be afraid to use the 'Contact' page to reach out to me for any coding needs
-            or business inquireies. Thanks for stopping by!</p>
+          <p id='biography'>
+            Hi there, and welcome to my portfolio. My name is Josh and I'm an aspiring junior developer! I started my coding journey Full Stack bootcamp
+            at UCF and for now, I am more of a front end developer but am currently working on my backend knowledge and honing my skills in Full Stack 
+            development with the MERN stack. If you'd like to see some of the projects i've worked on please visit the <a href='/projects'>projects</a> page 
+            and scroll through examples I've provided. A little bit about me, I was raised and currently live in a small town in Central Florida. I enjoy movies, anime,
+            and just about anything DC or Marvel (though DC definitely has the better stories). I try to stay active and like to run in obstacle course races like 
+            the Savage race and recently completed my first half marathon. Down below you can find links to a my current certifications. <br/>
+            Thanks for stopping by!
+            </p>
       </div>
       <Footer/>
     </>
--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -6,10 +6,10 @@ export default function Contact() {
   return (
     <>
     <Header/>
-    <p id='contact'>
-    <a href="mailto:jad24096@gmail.com"><button className='navButton'>SEND EMAIL</button></a><br/>
-    <a href='https://github.com/Jdiaz240'><button className='navButton'>MY GITHUB</button></a><br/>
-    </p>
+    <div id='contactInfo'>
+    <a id='contact' href="mailto:jad24096@gmail.com"><button id='contact' className='navButton'>SEND EMAIL</button></a><br/>
+    <a  href='https://github.com/Jdiaz240'><button className='navButton'>MY GITHUB</button></a>
+    </div>
     </>
   )
 }
